dde4d18d1932a21f92284fc480da711497d91949
rpsamples <redpandadocs@gmail.com>
2022-04-15 08:07:57 +0000

    Fixed issues in the script

--- delete_entry.py
+++ delete_entry.py
@@ -1,6 +1,6 @@
 import os
 from setup_spark import get_spark_session
-from handle_delta_tables import delete_entry 
+from handle_delta_tables import delete_entry, load_table_data
 
 spark = get_spark_session()
 file_with_names = open("names_to_be_deleted.txt", "r")
@@ -9,3 +9,6 @@ list2 = []
 for name in name_list:
     name = name.replace("\n", "")
     delete_entry(spark, name)
+
+output_df = load_table_data(spark)
+output_df.show()
--- restore_to_version.py
+++ restore_to_version.py
@@ -2,11 +2,11 @@ import os
 from setup_spark import get_spark_session
 from handle_delta_tables import restore_to_version
 
-#spark = get_spark_session()
 version = input("Version Number: ")
 try:
     version = int(version)
 except ValueError as e:
     print("Value passed is not int")
     exit()
+spark = get_spark_session()
 restore_to_version(spark, version)
